cabc4fe96189f7468411a310e154a36712dc71bb
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-02-18 12:55:04 +0700

    Update menu_reply.py

--- menu_reply.py
+++ menu_reply.py
@@ -1,4 +1,21 @@
 import random
+import emojis
+
+# function first reply bot saat start (/start)
+def botStart():
+	tentang_bot = '''Saya adalah bot Peribahasa Indonesia {0}.\
+	\nSaya bertugas untuk memberikan informasi\
+	\ntentang Peribahasa kepada kakak, agar\
+	\nkakak mendapat pengetahuan baru dan\
+	\nPeribahasa kita akan terus terlestarikan {1}.\
+	\n\nApa itu Peribahasa ? {2}\
+	\nMenurut Kamus Besar Bahasa Indonesia (KBBI),\
+	Peribahasa adalah kelompok kata atau kalimat\
+	yang tetap susunannya, biasanya mengiaskan\
+	maksud tertentu kak (dalam peribahasa termasuk\
+	juga bidal, ungkapan, dan perumpamaan).
+	'''.format(emojis.happy(), emojis.waving_hands[4], emojis.pointing_hands[4])
+	return tentang_bot
 
 # function random reply (balasan) bot (/hi)
 def botGreetings():
@@ -11,18 +28,18 @@ def botGreetings():
 
 # function tampilkan bot main menu (/menu)
 def botShowMenu():
-	list_menu = '''
-	/hi = Menyapa bot\
+	list_menu = '''LIST SELURUH MENU {}\
+	\n/hi = Menyapa bot\
 	\n/menu = Daftar seluruh menu\
 	\n/jenis = Jenis-jenis peribahasa\
-	'''
+	'''.format(emojis.pointing_hands[4])
 	return list_menu 
 
 # function tampilkan daftar (jenis-jenis) peribahasa (/peribahasa)
 def botShowPeribahasa():
-	list_peribahasa = '''
-	/peribahasa_bidal\n\
-	/peribahasa_pepatah\n\
-	/peribahasa_perumpamaan
-	'''
+	list_peribahasa = '''JENIS-JENIS PERIBAHASA {}\
+	\n/peribahasa_bidal\
+	\n/peribahasa_pepatah\
+	\n/peribahasa_perumpamaan\
+	'''.format(emojis.pointing_hands[4])
 	return list_peribahasa
