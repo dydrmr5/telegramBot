00591f80d18ceb8f029091d36a7e65931f9ad21d
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-02-23 01:05:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(peribahasa.bot_token, parse_mode = None)
 
 # ---------- MAIN MENU ----------
 # bot reply : perkenalan
-@bot.message_handler(commands = [f'{peribahasa.commands_telebot[0]}']) #start
+@bot.message_handler(commands = [f'{peribahasa.commands_telebot[0]}']) # /start
 def sentStartInfo(message):
 	#get information user
 	print(f"User ID  : {message.from_user.id}")
@@ -21,25 +21,25 @@ def sentStartInfo(message):
 	bot.reply_to(message, f'{menu_reply.botGreetings()} {user} {emojis.handWaves()}.\n{menu_reply.botStart()}')
 
 # bot reply : greetings/menyapa
-@bot.message_handler(commands = [f'{peribahasa.commands_telebot[1]}']) #hi
+@bot.message_handler(commands = [f'{peribahasa.commands_telebot[1]}']) # /hi
 def sentGreets(message):
 	user = message.from_user.first_name
 	bot.reply_to(message, f'{menu_reply.botGreetings()} {user} {emojis.handWaves()}.')
 
 # bot reply : menu (daftar menu bot)
-@bot.message_handler(commands = [f'{peribahasa.commands_telebot[2]}']) #menu
+@bot.message_handler(commands = [f'{peribahasa.commands_telebot[2]}']) # /menu
 def sentInfo(message):
 	bot.reply_to(message, menu_reply.botShowMenu())
 
 # bot reply : tampilkan menu (jenis-jenis) peribahasa
-@bot.message_handler(commands = [f'{peribahasa.commands_telebot[3]}']) #jenis
+@bot.message_handler(commands = [f'{peribahasa.commands_telebot[3]}']) # /jenis
 def sentMenu(message):
 	bot.reply_to(message, menu_reply.botShowPeribahasa())
 
 
 # ---------- menu (jenis-jenis) peribahasa ----------
 # bot reply : pilih jenis peribahasa menu
-@bot.message_handler(commands = [f'{peribahasa.commands_telebot[4]}']) #peribahasa
+@bot.message_handler(commands = [f'{peribahasa.commands_telebot[4]}']) # /peribahasa
 def showPeribahasa(message):
 	perintah = message.text[11:]
 	if 'bidal' in perintah.lower():
